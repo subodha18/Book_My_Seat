b57a1ebbec3a552cc957aa206644af8aa29c9395
subodha18 <subodhakumarsahoo14@gmail.com>
2026-01-21 18:56:24 +0530

    Renderingg in to another components

--- src/Components/Subus.jsx
+++ src/Components/Subus.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navba from './Home/Navba';
 import Footer from './Foot/Footer';
+import Apply_info from './Apply/Apply_info';
+import Addfe from './MidShow/Addfe';
+import Place from './Midshows/Place';
+import Ticket from './Ticket/Ticket';
+
 const Subus = () => {
   return (
     <div>
       <Navba />
+      <Ticket />
+      <Place />
+      <Addfe />
+      <Apply_info />
       <Footer />
     </div>
   )

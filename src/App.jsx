038e3696b826b22c05504072b388e873f073a9ec
subodha18 <subodhakumarsahoo14@gmail.com>
2026-02-20 09:17:05 +0530

    New Change in components import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import {createBrowserRouter,RouterProvider} from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
 import Login from './Components/First/Login';
 import User from './Components/use/User'
 import Owneri from './Components/Own/Owneri';
 import Info from './Components/Owninfo/Info';
-import Subus from './Components/subus';
+import Subus from './Components/Subus';
 import Buses from './Components/Bus_Root/Buses';
 import Seat from './Components/Bus_Root/Seat';
 import Confirmation from './Components/Bus_Root/Conformation';
